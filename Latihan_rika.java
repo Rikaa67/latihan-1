e5c787a930fd50690804f631780e4b7ac5150a60
rika <rikadoang72@gmail.com>
2023-11-01 23:51:57 +0700

    first commit

---
+++ Latihan_rika.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_rika;
+
+import java.util.Scanner;
+public class Latihan_rika {
+    
+     public static void main(String[] args) {
+        double Nilai1, Nilai2, hasil;
+        boolean hasil1;
+       
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        // OPERATOR BITWISE3
+        int Nilai3, Nilai4, hasil3;
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        
+//        // Bitwise and (&)
+//        hasil3 = Nilai3 & Nilai4;
+//        System.out.println("Bitwise and (&)");
+//        System.out.println("Nilai3 & Nilai4 = " + hasil3);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        
+//        // Bitwise OR (|)
+//        hasil3 = Nilai3 | Nilai4;
+//        System.out.println("Bitwise OR (|)");
+//        System.out.println("Nilai3 | Nilai4 = " + hasil3);
+//        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        System.out.print("Input angka-2: ");
+//        Nilai4 = keyboard.nextInt();
+//        
+//        // Bitwise XOR (^)
+//        hasil3 = Nilai3 ^ Nilai4;
+//        System.out.println("Bitwise XOR (^)");
+//        System.out.println("Nilai3 ^ Nilai4 = " + hasil3);
+//        
+        
+//         System.out.print("Input angka-1: ");
+//        Nilai4 = keyboard.nextInt();
+//        // Bitwise Negasi (~)
+//        hasil3 = ~ Nilai4;
+//        System.out.println("Bitwise Negasi (~)");
+//        System.out.println("~Nilai4 = " + hasil3);
+//        
+
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        // left shift
+//        hasil3 = Nilai3 <<  1;
+//        System.out.println("Bitwise left shift");
+//        System.out.println("Nilai3 << Nilai4 = " + hasil3);
+//        
+        
+//        System.out.print("Input angka-1: ");
+//        Nilai3 = keyboard.nextInt();
+//        // right shift
+//        hasil3 = Nilai3 >>  1;
+//        System.out.println("Bitwise right shift");
+//        System.out.println("Nilai3 >> Nilai4 = " + hasil3);
+
+
+        System.out.print("Input angka-1: ");
+        Nilai3 = keyboard.nextInt();
+        // left shift unsigned
+        hasil3 = Nilai3 >>> 2;
+        System.out.println("Bitwise left shift unsigned");
+        System.out.println("Nilai3 <<< Nilai4 = " + hasil3); 
+
+    
+
+    }
+    
+}
